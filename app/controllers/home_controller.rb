593c1b7da2b4569da8415d927d4de2b164bf7219
jinwoo KIm <woy131@naver.com>
2018-04-09 11:26:00 +0000

    fds

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,23 @@
+class HomeController < ApplicationController
+  def index
+    @post= Post.all
+  end
+
+  def create
+    @post = Post.new
+    @post.title = params[:title]
+    @post.content = params[:content]
+    @post.save
+    
+    redirect_to '/home/index'
+    
+  end
+
+  def new
+    
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+end
